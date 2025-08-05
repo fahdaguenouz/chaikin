973f17b99f0d53434d221a31e2553a1c5fa32cc6
fahdaguenouz <faguenouz@gmail.com>
2025-08-05 12:54:07 +0100

    changing from winit to macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,79 @@
-use winit::{
-    dpi::LogicalSize,
-    event::{ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit::dpi::PhysicalPosition;
-use pixels::{Pixels, SurfaceTexture};
+use macroquad::prelude::*;
 
-fn main() {
-    let event_loop = EventLoop::new();
-    let mut last_cursor_position: Option<PhysicalPosition<f64>> = None;
-    let mut positions: Vec<(f64, f64)> = vec![];
-
-    let window = WindowBuilder::new()
-        .with_title("Chaikin")
-        .with_inner_size(LogicalSize::new(800, 600))
-        .build(&event_loop)
-        .unwrap();
-
-    let window_size = window.inner_size();
-    let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-    let mut pixels = Pixels::new(window_size.width, window_size.height, surface_texture).unwrap();
+#[macroquad::main("Chaikin")]
+async fn main() {
+    let mut positions: Vec<Vec2> = Vec::new();
 
     println!("Window created! Press ESC to exit.");
     println!("Click to add points, press Enter to draw line between last 2 points, press C to clear.");
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    loop {
+        clear_background(BLACK);
 
-        match event {
-            Event::WindowEvent { event, window_id } if window_id == window.id() => {
-                match event {
-                    WindowEvent::CloseRequested => {
-                        println!("Close button clicked!");
-                        *control_flow = ControlFlow::Exit;
-                    }
-
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                virtual_keycode: Some(key_code),
-                                state: ElementState::Pressed,
-                                ..
-                            },
-                        ..
-                    } => {
-                        match key_code {
-                            VirtualKeyCode::Escape => {
-                                println!("Escape pressed, exiting...");
-                                *control_flow = ControlFlow::Exit;
-                            }
-                            VirtualKeyCode::Return => {
-                                if positions.len() < 2 {
-                                    println!(
-                                        "Need at least 2 points to draw a line! Current points: {}",
-                                        positions.len()
-                                    );
-                                } else {
-                                    pixels.frame_mut().fill(0);
-                                    let point1 = positions[positions.len() - 2];
-                                    let point2 = positions[positions.len() - 1];
-                                    println!(
-                                        "Drawing line from ({:.1}, {:.1}) to ({:.1}, {:.1})",
-                                        point1.0, point1.1, point2.0, point2.1
-                                    );
-                                }
-                            }
-                            VirtualKeyCode::C => {
-                                positions.clear();
-                                pixels.frame_mut().fill(0);
-                                window.request_redraw();
-                                println!("Cleared all points");
-                            }
-                            VirtualKeyCode::L => {
-                                if positions.len() >= 2 {
-                                   
-                                    println!(
-                                        "Drawing connected lines through {} points",
-                                        positions.len()
-                                    );
-                                } else {
-                                    println!("Need at least 2 points to draw connected lines!");
-                                }
-                            }
-                            _ => {
-                                println!("Key pressed: {:?}", key_code);
-                            }
-                        }
-                    }
-
-                    WindowEvent::MouseInput { state, button, .. } => {
-                        if let Some(position) = last_cursor_position {
-                            if state == ElementState::Pressed && button == MouseButton::Left {
-                                positions.push((position.x, position.y));
-                                println!(
-                                    "Point {} added at position: ({:.1}, {:.1})",
-                                    positions.len(),
-                                    position.x,
-                                    position.y
-                                );
-
-                                draw_dot(
-                                    pixels.frame_mut(),
-                                    window_size.width,
-                                    window_size.height,
-                                    (position.x, position.y),
-                                    3,
-                                );
-                                window.request_redraw();
-                            }
-                        }
-                    }
-
-                    WindowEvent::CursorMoved { position, .. } => {
-                        last_cursor_position = Some(position);
-                    }
-
-                    WindowEvent::Resized(new_size) => {
-                        if let Err(err) = pixels.resize_surface(new_size.width, new_size.height) {
-                            eprintln!("Resize error: {}", err);
-                            *control_flow = ControlFlow::Exit;
-                        }
-                    }
-
-                    _ => {}
-                }
-            }
-
-            Event::RedrawRequested(_) => {
-                if let Err(err) = pixels.render() {
-                    eprintln!("Render error: {}", err);
-                    *control_flow = ControlFlow::Exit;
-                }
-            }
-
-            Event::MainEventsCleared => {
-                // Redraw continuously if needed
-                // window.request_redraw();
-            }
-
-            _ => {}
+        // Handle input
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let pos = mouse_position().into();
+            positions.push(pos);
+            println!(
+                "Point {} added at position: ({:.1}, {:.1})",
+                positions.len(),
+                pos.x,
+                pos.y
+            );
         }
-    });
+
+        if is_key_pressed(KeyCode::Enter) {
+            if positions.len() < 2 {
+                println!(
+                    "Need at least 2 points to draw a line! Current points: {}",
+                    positions.len()
+                );
+            } else {
+                let point1 = positions[positions.len() - 2];
+                let point2 = positions[positions.len() - 1];
+                draw_line(point1.x, point1.y, point2.x, point2.y, 2.0, WHITE);
+                println!(
+                    "Drawing line from ({:.1}, {:.1}) to ({:.1}, {:.1})",
+                    point1.x, point1.y, point2.x, point2.y
+                );
+            }
+        }
+
+        if is_key_pressed(KeyCode::C) {
+            positions.clear();
+            println!("Cleared all points");
+        }
+
+        if is_key_pressed(KeyCode::L) {
+            if positions.len() >= 2 {
+                println!(
+                    "Drawing connected lines through {} points",
+                    positions.len()
+                );
+                for i in 0..positions.len() - 1 {
+                    let p1 = positions[i];
+                    let p2 = positions[i + 1];
+                    draw_line(p1.x, p1.y, p2.x, p2.y, 2.0, GRAY);
+                }
+            } else {
+                println!("Need at least 2 points to draw connected lines!");
+            }
+        }
+
+        if is_key_pressed(KeyCode::Escape) {
+            println!("Escape pressed, exiting...");
+            break;
+        }
+
+        // Draw dots for all positions
+        for pos in &positions {
+            draw_circle(pos.x, pos.y, 4.0, RED);
+        }
+
+        next_frame().await;
+    }
 }
+
 
 // Function to draw a small dot at clicked positions
 fn draw_dot(
